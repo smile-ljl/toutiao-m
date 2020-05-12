3b3ef9046c52df8bc1120c5e9881fb20cf8d91c4
unknown <1728918814@qq.com>
2020-05-12 17:51:43 +0800

    首页文章列表-页面布局-导航栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <van-nav-bar
+      class="app-nav-bar"
+    >
+      <!-- 导航栏 -->
+      <van-button
+        class="search-btn"
+        slot="title"
+        type="info"
+        size="small"
+        round
+        icon="search"
+      >搜索</van-button>
+      <!-- /导航栏 -->
+    </van-nav-bar>
+  </div>
 </template>
 
 <script>
@@ -18,4 +33,24 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.home-container {
+  .app-nav-bar {
+    /deep/ .van-nav-bar__title {
+      max-width: none;
+    }
+    .search-btn {
+      background-color: #5babfb;
+      width: 277px;
+      height: 32px;
+      border: none;
+      .van-button__text {
+        font-size: 14px;
+      }
+      .van-icon {
+        font-size: 16px;
+      }
+    }
+  }
+}
+</style>
