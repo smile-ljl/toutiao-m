537c446dc5c2f718f884dc4d6bf4d80b20a91e18
unknown <1728918814@qq.com>
2020-05-17 09:23:53 +0800

    频道编辑-页面布局-样式

---
+++ src/views/home/comments/channel-edit.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="channel-edit">
+    <van-cell-group>
+      <!-- 我的频道 -->
+      <van-cell center :border="false">
+        <div
+          slot="title"
+          class="channel-title"
+        >我的频道</div>
+        <van-button
+          type="danger"
+          plain
+          round
+          size="mini"
+        >编辑</van-button>
+      </van-cell>
+      <van-grid :gutter="10">
+        <van-grid-item
+          class="grid-item"
+          v-for="value in 8"
+          :key="value"
+          text="文字"
+        />
+      </van-grid>
+      <!-- /我的频道 -->
+
+      <!-- 推荐频道 -->
+      <van-cell center :border="false">
+        <div class="channel-title" slot="title">频道推荐</div>
+      </van-cell>
+      <van-grid :gutter="10">
+        <van-grid-item
+          class="grid-item"
+          v-for="value in 8"
+          :key="value"
+          text="文字"
+        />
+      </van-grid>
+      <!-- /推荐频道 -->
+    </van-cell-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChannelEdit',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less">
+.channel-edit {
+  padding-top: 54px;
+}
+.channel-title {
+    font-size: 16px;
+    color: #333333;
+  }
+  .grid-item {
+    width: 80px;
+    height: 43px;
+    /deep/ .van-grid-item__content {
+      background-color: #f4f5f6;
+      .van-grid-item__text {
+        font-size: 14px;
+        color: #222;
+      }
+    }
+  }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -15,7 +15,10 @@
       </van-nav-bar>
       <!-- /导航栏 -->
       <!-- 文章频道 -->
-      <van-tabs v-model="active">
+      <van-tabs
+        class="channel-tabs"
+        v-model="active"
+      >
         <van-tab
           :title="channel.name"
           v-for="channel in channels"
@@ -23,24 +26,55 @@
         >
           <article-list :channel="channel" />
         </van-tab>
+
+        <div
+          class="wap-nav-placeholder"
+          slot="nav-right"
+        ></div>
+        <!-- 汉堡按钮 -->
+        <div
+          class="wap-nav-wrap"
+          slot="nav-right"
+          @click="isChannelEditShow = true"
+        >
+          <van-icon name="wap-nav" />
+        </div>
+        <!-- /汉堡按钮 -->
       </van-tabs>
       <!-- /文章频道 -->
+
+      <!-- 文章编辑弹出层 -->
+      <van-popup
+        class="channel-edit-popup"
+        v-model="isChannelEditShow"
+        position="bottom"
+        style="height: 100%"
+        closeable
+        close-icon-position="top-left"
+        get-container="body"
+      >
+        <channel-edit />
+      </van-popup>
+      <!-- /文章编辑弹出层 -->
   </div>
 </template>
 
 <script>
 import { getUsreChannels } from '@/api/user'
 import ArticleList from './comments/article-list'
+import ChannelEdit from './comments/channel-edit'
 export default {
   name: 'HomeIndex',
   components: {
-    ArticleList
+    ArticleList,
+    ChannelEdit
   },
   props: {},
   data () {
     return {
       active: 0, // 控制被激活的标签
-      channels: []
+      channels: [],
+      isChannelEditShow: false
     }
   },
   computed: {},
@@ -75,6 +109,46 @@ export default {
       .van-icon {
         font-size: 16px;
       }
+    }
+  }
+  .channel-tabs {
+    /deep/ .van-tab {
+      border-right: 1px solid #edeff3;
+      border-bottom: 1px solid #edeff3;
+    }
+    /deep/ .van-tabs__line {
+      bottom: 20px;
+      width: 15px !important;
+      height: 3px;
+      background: #3296fa;
+    }
+  }
+  .wap-nav-placeholder {
+    width: 33px;
+    flex-shrink: 0;
+  }
+  .wap-nav-wrap {
+    position: fixed;
+    right: 0;
+    width: 33px;
+    height: 43px;
+    background-color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: .9;
+    .van-icon {
+      font-size: 24px;
+    }
+    &:before {
+      content: '';
+      width: 1px;
+      background: url('./line.png') no-repeat;
+      background-size: contain;
+      position: absolute;
+      left: 0;
+      top: 0;
+      bottom: 0;
     }
   }
 }
